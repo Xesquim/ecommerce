ed3551300d40b56b182cb550f46d8021e42abb90
Xesquim <matheus.g.cesquim@gmail.com>
2023-08-22 18:14:49 -0300

    Adicionando JWT

--- api/EcommerceWebAPI/Program.cs
+++ api/EcommerceWebAPI/Program.cs
@@ -1,4 +1,10 @@
 using EcommerceWebAPI.Data;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using EcommerceWebAPI.Security;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +17,54 @@ builder.Services.AddControllers()
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme {
+                Reference = new OpenApiReference {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            }, new List<string>()
+        }
+    });
+});
+
+var signingConfiguration = new SigningConfigurations();
+builder.Services.AddSingleton<SigningConfigurations>(signingConfiguration);
+
+builder.Services.AddAuthentication(authOptions =>
+{
+    authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(bearerOptions =>
+{
+    var paramsValidation = bearerOptions.TokenValidationParameters;
+    paramsValidation.IssuerSigningKey = signingConfiguration.Key;
+    paramsValidation.ValidAudience = Environment.GetEnvironmentVariable("AUDIENCE");
+    paramsValidation.ValidIssuer = Environment.GetEnvironmentVariable("ISSUER");
+    paramsValidation.ValidateIssuerSigningKey = true;
+    paramsValidation.ValidateLifetime = true;
+    paramsValidation.ClockSkew = TimeSpan.Zero;
+});
+
+builder.Services.AddAuthorization(auth =>
+{
+    auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+        .RequireAuthenticatedUser().Build());
+});
 
 var app = builder.Build();
 
@@ -24,5 +77,17 @@ if (app.Environment.IsDevelopment())
 app.UseAuthorization();
 
 app.MapControllers();
+
+if (Environment.GetEnvironmentVariable("APPLY_MIGRATIONS")?.ToLower() == "true")
+{
+    using (var service = app.Services.GetRequiredService<IServiceScopeFactory>()
+                                                .CreateScope())
+    {
+        using (var context = service.ServiceProvider.GetService<EcommerceContext>())
+        {
+            context?.Database.Migrate();
+        }
+    }
+}
 
 app.Run();
